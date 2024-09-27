15f60a10bc418d152dda1bbada9d907908f8b4b5
LucianoCseke <165181674+LucianoCseke@users.noreply.github.com>
2024-09-27 00:17:28 -0300

    Dockerfile add

--- AmecysWebAPI/AmecysAPI.DataAccessLayer/obj/Debug/net8.0/AmecysAPI.DataAccessLayer.AssemblyInfo.cs
+++ AmecysWebAPI/AmecysAPI.DataAccessLayer/obj/Debug/net8.0/AmecysAPI.DataAccessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AmecysAPI.DataAccessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41e95e54f07ced221f01c285010927b699ae843c")]
 [assembly: System.Reflection.AssemblyProductAttribute("AmecysAPI.DataAccessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AmecysAPI.DataAccessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
